794c2065ba310db465a42cd422ff8bd707bf0d52
geovan22 <geovan221315@hotmail.com>
2021-06-09 11:22:10 -0600

    crud

--- VecPCC/VecPCC/Program.cs
+++ VecPCC/VecPCC/Program.cs
@@ -10,6 +10,7 @@ namespace VecPCC
         static void Main(string[] args)
         {
             double[] dato = new double[10];//0 -  n
+            double buscar;
 
             //carga de datos
             for (int i = 0; i<10; i++)
@@ -18,7 +19,24 @@ namespace VecPCC
                 Console.Write("ingrese un valor: ");
                 dato[i] = double.Parse(Console.ReadLine());   
             }
+            Console.WriteLine("----------------------------");
+            for (int x=0; x<10; x++)
+            {
+                Console.WriteLine(dato[x]);
+            }
 
+            Console.WriteLine("----------------------------");
+
+            Console.Write("ingrese el valor a buscar: ");
+            buscar = double.Parse(Console.ReadLine());
+
+            for (int z=0; z<10;z++)
+            {
+                if (buscar==dato[z])
+                {
+                    Console.WriteLine("el dato " + buscar + " existe");
+                }
+            }
 
             Console.ReadKey();
         }
